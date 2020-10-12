f2727cb9bbb020a98a70eff0e48319652da1384c
xms363 <72204373+xms363@users.noreply.github.com>
2020-10-12 22:13:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,53 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+disablePkgsList="
+./feeds/lienol/package/chinadns-ng 
+./feeds/lienol/package/ipt2socks 
+./feeds/lienol/others/luci-app-kcptun 
+./feeds/lienol/lienol/luci-app-ssr-python-pro-server 
+./package/lean/luci-app-kodexplorer 
+./package/lean/luci-lib-docker 
+./package/lean/luci-app-dockerman 
+./package/lean/kcptun 
+./package/lean/luci-theme-argon 
+./package/lean/v2ray 
+./package/lean/v2ray-plugin 
+./feeds/packages/net/miniupnpd 
+./feeds/packages/net/mwan3 
+./feeds/packages/utils/ttyd 
+./feeds/packages/utils/docker-ce 
+./feeds/packages/utils/containerd 
+./feeds/packages/utils/libnetwork 
+./feeds/packages/utils/tini 
+./feeds/packages/net/dnscrypt-proxy2 
+./feeds/packages/net/https-dns-proxy 
+./feeds/packages/net/kcptun 
+./feeds/packages/net/smartdns 
+./package/network/services/dropbear 
+./feeds/luci/applications/luci-app-upnp 
+./feeds/luci/applications/luci-app-sqm
+"
+
+function disableDulicatedPkg()
+{
+	if [ -d $1 ];then
+		rm -rf $1
+		echo $1" Disabled."
+	fi
+}
+
+./scripts/feeds update -a
+
+for disablePkg in $disablePkgsList
+do
+	disableDulicatedPkg $disablePkg
+done
+
+./scripts/feeds update -i
+./scripts/feeds install -a
+
+#if [ ! -f .config ];then
+#cp defconfig .config
+#echo "Default .config created."
+#fi
